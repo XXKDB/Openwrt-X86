80fcae7564801aa831c8045d1a7aec94854bdd7a
XXKDB <82255597+XXKDB@users.noreply.github.com>
2021-06-18 19:09:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,6 +27,7 @@ rm -rf package/lean/luci-theme-material
 rm -rf package/lean/luci-theme-netgear
 rm -rf package/kenzo/luci-app-argon-config
 rm -rf package/kenzo/luci-theme-argon_new
+rm -rf package/kenzo/luci-app-advanced
 
 #下载主题luci-theme-argon
 git clone https://github.com/XXKDB/luci-theme-argon_armygreen.git package/lean/luci-theme-argon_armygreen
